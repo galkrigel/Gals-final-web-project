fa72b9487a12176e3824087f7b3702f077e6cdf7
galkrigel <galkrigel@gmail.com>
2024-02-04 18:23:29 +0200

    add multer

--- server/webserver/dist/src/routes/auth_route.js
+++ server/webserver/dist/src/routes/auth_route.js
@@ -12,7 +12,43 @@ const auth_controller_1 = __importDefault(require("../controllers/auth_controlle
 *   name: Auth
 *   description: The Authentication API
 */
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ */
 router.post("/register", auth_controller_1.default.register);
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Log in a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ */
 router.post("/login", auth_controller_1.default.login);
 router.get("/logout", auth_controller_1.default.logout);
 router.get("/refresh", auth_controller_1.default.refresh);
--- server/webserver/src/routes/auth_route.ts
+++ server/webserver/src/routes/auth_route.ts
@@ -10,7 +10,46 @@ import authController from "../controllers/auth_controller";
 *   description: The Authentication API
 */
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ */
+
 router.post("/register", authController.register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Log in a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ */
+
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 router.get("/refresh", authController.refresh);
